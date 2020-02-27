da8e5060da2a058ee903f81ae16700ab538ab423
haltosan <32181538+haltosan@users.noreply.github.com>
2020-02-27 04:40:12 +0000

    create main.py from repl

---
+++ main.py
@@ -0,0 +1,13 @@
+def makeMem():
+  a = []
+  a.append(1)
+  for i in range(1,75):
+    a.append(0)
+  a.append(-1)
+  for i in range(0,255):
+    a.append(i)
+    
+  for i in range(328,585):
+    a.append(0)
+  print((a))
+  
